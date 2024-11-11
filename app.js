67103eeffdb824612c8a01f577a8d16111370caf
keshu <keshujangid191@gmail.com>
2024-11-11 08:01:26 +0530

    change Database URL

--- app.js
+++ app.js
@@ -77,5 +77,5 @@ app.use((err, req, res, next) => {
 });
 
 
-
-app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
