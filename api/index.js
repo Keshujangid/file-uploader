ee5222aa570b0e507d696c990c79dfdba368e66f
keshu <keshujangid191@gmail.com>
2024-11-12 10:08:10 +0530

    add vercel deployment files

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const expressSession = require("express-session");
 const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const { PrismaClient } = require('@prisma/client');
-const passport = require('./middleware/passport');
+const passport = require('../middleware/passport');
 const path = require('path');
 // const route = require('./routes/route');
-const authRoutes = require('./routes/auth');
-const driveRoutes = require('./routes/drive');
-const shareRoutes = require('./routes/share');
+const authRoutes = require('../routes/auth');
+const driveRoutes = require('../routes/drive');
+const shareRoutes = require('../routes/share');
 const flash = require('connect-flash');
 
 const app = express();
@@ -16,9 +16,9 @@ const app = express();
 
 
 // Set view engine
-app.set("views", path.join(__dirname, "./views")); // Update path
+app.set("views", path.join(__dirname, "../views")); // Update path
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.use(
